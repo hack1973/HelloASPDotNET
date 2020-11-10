9fcabf576e780b8eefe9f9f3d18cf847fac53aa3
Michael Hacker <hack1973@swbell.net>
2020-11-10 10:13:39 -0600

    completed CH 10 follow along coding

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNET.Controllers
+{
+    [Route("/helloworld")]
+    public class HelloController : Controller
+    {
+        // GET: /helloworld
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<form method='post'action='/helloworld/welcome'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
+            return Content(html, "text/html");
+        }
+
+        // GET: /helloworld or /helloworld/welcome or /helloworld/welcome/name or /helloworld/welcome?name=name        
+        [HttpGet("welcome/{name?}")]
+        [HttpPost("welcome")]      
+        public IActionResult Welcome(string name="World")
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+    }
+}
